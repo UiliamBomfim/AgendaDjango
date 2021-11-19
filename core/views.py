2ff5010c824e87f023b4861070b5b38888b6925a
Uiliam Bomfim <wf3@outlook.com.br>
2021-11-19 00:50:26 -0300

    add listagem

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ from core.models import Evento
 # Create your views here.
 
 def lista_eventos(request):
+    #usuario = request.user
+    #evento = Evento.objects.filter(usuario=usuario)
     evento = Evento.objects.all()
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
